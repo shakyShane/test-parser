0539a6469517d7c0b1d71e6f59aa177842f3fcd3
Shane Osbourne <shane.osbourne8@gmail.com>
2015-12-25 20:16:37 +0000

    Fix example file

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-var htmlparser = require("./tokenizer");
+var htmlparser = require("./index");
 var fs = require('fs');
 var hb = require('handlebars');
 var string = fs.readFileSync('test.html', 'utf8');
@@ -10,74 +10,8 @@ console.time('hb');
 hb.compile(string)({});
 console.timeEnd('hb');
 
-function parse (string) {
-    var stack = [];
-    var current;
-    var nextattr;
-    htmlparser.parse(string, {
-        ontext: function (text, loc) {
-
-            //console.log('sub', [string.substring(loc.startIndex, loc.endIndex)]);
-
-            stack.push({
-                type: 'TEXT',
-                value: text,
-                loc: {
-                    start: loc.startIndex,
-                    end: loc.endIndex
-                }
-            });
-            //console.log('TEXT:', [text]);
-        },
-        onopentagname: function (name, loc) {
-            //console.log('open tag', [string.substring(loc.startIndex-2, loc.endIndex+2)]);
-            current = {
-                type: 'OPEN_TAG',
-                value: name,
-                attrs: {},
-                loc: {
-                    start: loc.startIndex - 2,
-                    end: loc.endIndex + 2
-                }
-            };
-        },
-        onopentagend: function (loc) {
-            current.loc.end = loc.endIndex + 2;
-            stack.push(current);
-            current = undefined;
-        },
-        onclosetag: function (name, loc) {
-
-            stack.push({
-                type: "CLOSE_TAG",
-                value: name,
-                loc: {
-                    start: loc.startIndex - 3,
-                    end: loc.endIndex + 2
-                }
-            });
-            //stack.push(current);
-            current = undefined;
-        },
-        onselfclosingtag: function () {
-
-        },
-        onattribname: function (nae) {
-            nextattr = nae;
-            current.attrs[nae] = '';
-        },
-        onattribend: function () {
-
-        },
-        onattribdata: function (value) {
-            if (!current) return;
-            current.attrs[nextattr] = value;
-        }
-    });
-    return stack;
-}
-
 console.time('shane');
-var ast = parse(string);
+var ast = htmlparser.parse(string);
+//fs.writeFileSync('ast2.json', JSON.stringify(ast, null, 2));
 console.timeEnd('shane');
 //module.exports.parse = parse;
